89f660e0a58d909596ccc7ac4c3758e9b8575f63
Paige Williams <paw145@humboldt.edu>
2019-03-30 20:09:07 -0700

    Modify styling in layout and header.

---
+++ packages/theme/src/components/header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default ({ children }) => (
+  <section
+    style={{
+      padding: "20px",
+      backgroundColor: "#696969"
+    }}
+  >
+    {children}
+  </section>
+);
--- packages/theme/src/components/layout.js
+++ packages/theme/src/components/layout.js
@@ -1,8 +1,12 @@
+import React from "react";
+
 export default ({ children }) => (
   <div
     style={{
       margin: "10%",
-      backgroundColor: "tomato"
+      backgroundColor: "#ff6347",
+      height: "100vh",
+      width: "100vw"
     }}
   >
     {children}
